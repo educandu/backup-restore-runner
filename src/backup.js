6d0d1bf4b31660640e40ad4bad6217474595e0c4
Adriana Luchian <adriana.dumitras@gmail.de>
2021-11-04 10:23:41 +0100

    EDU-31 Correct failure messages for backup runner

--- src/backup.js
+++ src/backup.js
@@ -32,8 +32,8 @@ module.exports.backup = async () => {
       });
     } catch (error) {
       const database = stringHelper.getDatabaseNameFromUri(env.mongoDbUri);
-      console.log('Failed to restore mongoDB', error);
-      await slackClient.notify(`Failed to restore mongoDB _${database}_`, error);
+      console.log('Failed to back up mongoDB', error);
+      await slackClient.notify(`Failed to back up mongoDB _${database}_`, error);
       return false;
     }
     return true;
@@ -48,8 +48,8 @@ module.exports.backup = async () => {
         backupBucketFolder
       });
     } catch (error) {
-      console.log('Failed to restore mongoDB S3 bucket', error);
-      await slackClient.notify(`Failed to restore S3 bucket _${env.bucketName}_`, error);
+      console.log('Failed to back up mongoDB S3 bucket', error);
+      await slackClient.notify(`Failed to back up S3 bucket _${env.bucketName}_`, error);
       return false;
     }
     return true;
